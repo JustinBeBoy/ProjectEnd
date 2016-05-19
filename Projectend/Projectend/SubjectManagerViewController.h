bdeb291eed0b7c3308a79864d36225d8cd7cd6a1
Ominext Mobile <justin@gmail.com>
2016-05-19 08:36:16 +0700

    NamDT - Add file to xcodeproject

---
+++ Projectend/Projectend/SubjectManagerViewController.h
@@ -0,0 +1,13 @@
+//
+//  SubjectManagerViewController.h
+//  Projectend
+//
+//  Created by Ominext Mobile on 5/19/16.
+//  Copyright © 2016 Ominext Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SubjectManagerViewController : UIViewController
+
+@end
---
+++ Projectend/Projectend/SubjectManagerViewController.m
@@ -0,0 +1,37 @@
+//
+//  SubjectManagerViewController.m
+//  Projectend
+//
+//  Created by Ominext Mobile on 5/19/16.
+//  Copyright © 2016 Ominext Mobile. All rights reserved.
+//
+
+#import "SubjectManagerViewController.h"
+
+@interface SubjectManagerViewController ()
+
+@end
+
+@implementation SubjectManagerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
